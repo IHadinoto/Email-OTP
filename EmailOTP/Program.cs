868683fe1e376fd04e381398f9a96e15ff029e6c
Ian Nathanael Hadinoto <ianhadinoto@gmail.com>
2024-11-19 16:59:48 +1100

    Initial Code

---
+++ EmailOTP/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class Email_OTP_Module
+{
+    private static readonly string DOMAIN = "@dso.org.sg";
+    private const int OTP_LENGTH = 6;
+    private const int OTP_VALIDITY_DURATION = 60000; // 1 minute in milliseconds
+    private const int MAX_OTP_ATTEMPTS = 10;
+
+    private string currentOTP;
+    private DateTime otpGeneratedTime;
+    private int otpAttempts;
+
+    public void start()
+    {
+        // Initialize variables if necessary
+    }
+
+    public void close()
+    {
+        // Clean up resources if necessary
+    }
+
+    public string GenerateOTP()
+    {
+        Random random = new Random();
+        string otp = random.Next(0, 999999).ToString("D6");
+        return otp;
+    }
+
+    public bool ValidateEmailDomain(string email)
+    {
+        return email.EndsWith(DOMAIN, StringComparison.OrdinalIgnoreCase);
+    }
+
+    public bool SendEmail(string emailAddress, string emailBody)
+    {
+        try
+        {
+            // Assuming send_email is defined elsewhere
+            send_email(emailAddress, emailBody);
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    public int generate_OTP_email(string user_email)
+    {
+        if (!Regex.IsMatch(user_email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+        {
+            return STATUS_EMAIL_INVALID;
+        }
+
+        if (!ValidateEmailDomain(user_email))
+        {
+            return STATUS_EMAIL_INVALID;
+        }
+
+        currentOTP = GenerateOTP();
+        otpGeneratedTime = DateTime.Now;
+        otpAttempts = 0;
+
+        string emailBody = $"Your OTP Code is {currentOTP}. The code is valid for 1 minute";
+
+        if (SendEmail(user_email, emailBody))
+        {
+            return STATUS_EMAIL_OK;
+        }
+        else
+        {
+            return STATUS_EMAIL_FAIL;
+        }
+    }
+
+    public int check_OTP(TextReader input)
+    {
+        var cts = new CancellationTokenSource(OTP_VALIDITY_DURATION);
+        var token = cts.Token;
+
+        while (otpAttempts < MAX_OTP_ATTEMPTS && DateTime.Now - otpGeneratedTime < TimeSpan.FromMilliseconds(OTP_VALIDITY_DURATION))
+        {
+            try
+            {
+                string userInput = Task.Run(() => input.ReadLine(), token).Result;
+                if (string.IsNullOrEmpty(userInput))
+                {
+                    continue;
+                }
+
+                if (userInput == currentOTP)
+                {
+                    return STATUS_OTP_OK;
+                }
+
+                otpAttempts++;
+            }
+            catch (OperationCanceledException)
+            {
+                return STATUS_OTP_TIMEOUT;
+            }
+        }
+
+        if (otpAttempts >= MAX_OTP_ATTEMPTS)
+        {
+            return STATUS_OTP_FAIL;
+        }
+
+        return STATUS_OTP_TIMEOUT;
+    }
+
+    private void send_email(string emailAddress, string emailBody)
+    {
+        // Implementation for sending email
+        // Assuming this function is defined elsewhere or can use System.Net.Mail
+        using (var client = new SmtpClient("smtp.yourserver.com"))
+        {
+            var mail = new MailMessage("your-email@dso.org.sg", emailAddress)
+            {
+                Subject = "Your OTP Code",
+                Body = emailBody
+            };
+            client.Send(mail);
+        }
+    }
+
+    public const int STATUS_EMAIL_OK = 0;
+    public const int STATUS_EMAIL_FAIL = 1;
+    public const int STATUS_EMAIL_INVALID = 2;
+    public const int STATUS_OTP_OK = 3;
+    public const int STATUS_OTP_FAIL = 4;
+    public const int STATUS_OTP_TIMEOUT = 5;
+}
+
+namespace OTPModuleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Email_OTP_Module otpModule = new Email_OTP_Module();
+
+            otpModule.start();
+
+            Console.WriteLine("Enter your email address (only @dso.org.sg domain is allowed):");
+            string userEmail = Console.ReadLine();
+
+            int emailStatus = otpModule.generate_OTP_email(userEmail);
+            if (emailStatus == Email_OTP_Module.STATUS_EMAIL_OK)
+            {
+                Console.WriteLine("OTP has been sent to your email. Please check your email and enter the OTP below:");
+            }
+            else if (emailStatus == Email_OTP_Module.STATUS_EMAIL_INVALID)
+            {
+                Console.WriteLine("The email address is invalid.");
+                otpModule.close();
+                return;
+            }
+            else if (emailStatus == Email_OTP_Module.STATUS_EMAIL_FAIL)
+            {
+                Console.WriteLine("Failed to send OTP to the email address.");
+                otpModule.close();
+                return;
+            }
+
+            using (StringReader reader = new StringReader(Console.ReadLine()))
+            {
+                int otpStatus = otpModule.check_OTP(reader);
+                if (otpStatus == Email_OTP_Module.STATUS_OTP_OK)
+                {
+                    Console.WriteLine("OTP is valid and checked.");
+                }
+                else if (otpStatus == Email_OTP_Module.STATUS_OTP_FAIL)
+                {
+                    Console.WriteLine("Failed to enter valid OTP after 10 tries.");
+                }
+                else if (otpStatus == Email_OTP_Module.STATUS_OTP_TIMEOUT)
+                {
+                    Console.WriteLine("OTP entry timed out after 1 minute.");
+                }
+            }
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+
+            otpModule.close();
+        }
+    }
+}
---
+++ EmailOTP/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EmailOTP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmailOTP")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("62d8367a-48c1-4f9e-9f07-9757c62f93ad")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
